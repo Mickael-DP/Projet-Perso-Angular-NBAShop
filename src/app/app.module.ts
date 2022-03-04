fb936dcac479def1c4307e0a7d7899e27a9d5bb6
Mickael-DP <m.dallepasqualine@gmail.com>
2022-03-04 09:21:19 +0100

    add product card v2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule, LOCALE_ID } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import localeFr from '@angular/common/locales/fr';
 import { AppComponent } from './app.component';
-import { ProductCardComponent } from './product-card/product-card.component';
+import { ProductCardComponent} from "./product-card/product-card.component";
 import {SortByDatePipe} from './pipes/product.pipe'
 import {HeaderComponent} from "./header/header.component";
 registerLocaleData(localeFr);
